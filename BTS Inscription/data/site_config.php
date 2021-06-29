8a256625070a432f57114e67939729894473dc88
Aya Ait <ayabug89@gmail.com>
2021-06-29 22:18:53 +0100

    credits year

--- BTS Inscription/data/site_config.php
+++ BTS Inscription/data/site_config.php
@@ -2,7 +2,7 @@
 //Set a date to finish applying
 $countDays=countDays(1);
 
-//Set number of seats for main bacType anD autres
+//Set number of seats for main bacType and autres
 define("BACMAIN", 23);
 define("BACAUTRES",7);
 ?>
